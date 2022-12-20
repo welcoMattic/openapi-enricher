a799514c611cd95d077db449f6ffb402cad32ef5
Mathieu Santostefano <msantostefano@protonmail.com>
2022-12-20 11:17:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# OpenAPI Enricher
+
+> Enrich an OpenAPI spec with response examples
+
+## Usage
+
+1. Install openapi-enricher
+2. Write your response exemples in a JSON file, following the provided template (examples.json)
+3. Run `openapi-enricher openapi.json -e examples.json -o enriched.json`
+
+## Limitations
+
+* It supports only JSON OpenAPI spec (for now)
+* It support only examples for response in paths, not for components
+
+## Credits
+
+* [All contributors](https://github.com/welcomattic/openapi-enricher/graphs/contributors)
+
+## License
+
+OpenAPI Enricher is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ openapi-enricher.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+"use strict"
+
+const fs = require('fs')
+const program = require('commander')
+
+program
+    .arguments('<oaFile>')
+    .usage('<file> [options]')
+    .description(require('./package.json').description)
+    .option('-o, --output <output>', 'save the enriched OpenAPI file as JSON')
+    .option('-e, --examplesFile <examplesFile>', 'the file to specify HTTP response examples')
+    .version(require('./package.json').version, '--version')
+    .option('-v, --verbose', 'verbosity that can be increased', increaseVerbosity, 0)
+    .action(run)
+    .exitOverride((err) => {
+        if (
+            err.code === "commander.missingArgument" ||
+            err.code === "commander.unknownOption"
+        ) {
+            process.stdout.write("\n")
+            program.outputHelp()
+        }
+
+        process.exit(err.exitCode)
+    })
+    .parse(process.argv)
+
+function increaseVerbosity(dummyValue, previous) {
+    return previous + 1
+}
+
+async function run(oaFile, options) {
+    let oa = JSON.parse(fs.readFileSync(oaFile, 'utf8'))
+
+    let examples = JSON.parse(fs.readFileSync(options.examplesFile, 'utf8'))
+    Object.entries(oa.paths).forEach(([path, methods]) => {
+        Object.entries(methods).forEach(([method, methodOptions]) => {
+            oa.paths[path][method] = Object.assign({}, methodOptions, examples.paths[path][method])
+        });
+    });
+
+    let output = JSON.stringify(oa, null, 2)
+
+    if (options.output) {
+        try {
+            fs.writeFileSync(options.output, output, 'utf8')
+            console.info(`- Output file:\t\t${options.output}`)
+        } catch (err) {
+            console.error('\x1b[31m', `Output file error - no such file or directory "${options.output}"`)
+            if (options.verbose >= 1) {
+                console.error(err)
+            }
+        }
+    } else {
+        console.log(output)
+    }
+}
