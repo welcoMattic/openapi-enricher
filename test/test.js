0ac2ddd3f3a332c34e68c59f39a1dd0580257137
Mathieu Santostefano <msantostefano@protonmail.com>
2023-01-20 12:44:15 +0100

    feat(yaml): add YAML support

--- test/test.js
+++ test/test.js
@@ -2,11 +2,37 @@ const { exec } = require('child_process')
 const path = require('path')
 const fs = require('fs')
 
-test('_', (done) => {
-    exec(`node ${path.resolve('./openapi-enricher')} ${['./test/fixtures/petstore.json', `--examplesFile ./test/fixtures/petstore-examples.json`].join(' ')}`,
+test('_json', (done) => {
+    exec(`node ${path.resolve('./openapi-enricher')} ${[
+        `./test/fixtures/json/petstore.json`,
+        `--examplesFile ./test/fixtures/json/petstore-examples.json`
+    ].join(' ')}`,
         (err, out) => {
-            expect(out.toString()).toBe(fs.readFileSync('./test/fixtures/enriched.json', 'utf8'))
-            done();
+            expect(out.toString()).toBe(fs.readFileSync('./test/fixtures/json/enriched.json', 'utf8'))
+            done()
         }
-    );
+    )
+})
+
+test('_yaml', (done) => {
+    exec(`node ${path.resolve('./openapi-enricher')} ${[
+            `./test/fixtures/yaml/petstore.yaml`,
+            `--examplesFile ./test/fixtures/yaml/petstore-examples.yaml`,
+            `--format yaml`
+        ].join(' ')}`,
+        (err, out) => {
+            expect(out.toString()).toBe(fs.readFileSync('./test/fixtures/yaml/enriched.yaml', 'utf8'))
+            done()
+        }
+    )
+    exec(`node ${path.resolve('./openapi-enricher')} ${[
+            `./test/fixtures/yaml/petstore.yml`,
+            `--examplesFile ./test/fixtures/yaml/petstore-examples.yml`,
+            `--format yml`
+        ].join(' ')}`,
+        (err, out) => {
+            expect(out.toString()).toBe(fs.readFileSync('./test/fixtures/yaml/enriched.yml', 'utf8'))
+            done()
+        }
+    )
 })
